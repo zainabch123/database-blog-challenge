ab520742bec9f76692658cf8bd8f9c4c2bf083ef
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-08-09 15:16:41 +0100

    Created user model and updated data seed

---
+++ prisma/migrations/20240809141225_user/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `username` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(10)`.
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "email" TEXT NOT NULL,
+ALTER COLUMN "username" SET DATA TYPE VARCHAR(10);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- prisma/seed.js
+++ prisma/seed.js
@@ -3,10 +3,11 @@ const prisma = new PrismaClient();
 
 async function seed() {
     const createdUsers = await prisma.user.createMany({
-        data: [
-            { username: 'alicemartin' },
-            { username: 'alicemartin' }
-        ]
+      data: [
+        { username: "alicemartn", email: "aliceMartin@gmail.com" },
+        { username: "john_smith", email: "johnSmith@gmail.com" },
+        { username: "dorisP12", email: "dorisParker@gmail.com" },
+      ],
     });
 
     console.log(`${createdUsers.count} users created`, createdUsers);
